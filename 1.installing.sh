ff1c5d27c5de8009d6bc7950bf72758fd40a9dd8
Venkat53253 <venkatratna532@gmail.com>
2025-05-22 11:45:09 +0530

    hello worlds created

--- 1.installing.sh
+++ 1.installing.sh
@@ -11,9 +11,11 @@ dnf -y install mysql-serverbhbhbhbh
 if [ $? -ne 0 ]; then
     echo "Failed to install MySQL server"
     exit 10
+    fi
     dnf -y install nginx njnjngin
     if [ $? -ne 0 ]; then
         echo "Failed to install Nginx"
         exit 20
     fi
+
 
